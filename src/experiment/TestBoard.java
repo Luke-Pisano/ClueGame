b22fea66aec039f45ac2748a99f3f9d7a07a8321
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2025-03-03 16:18:51 -0700

    Passes all tests worked together on same device

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,84 +1,96 @@
 package experiment;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class TestBoard {
-    private final int NUMROWS = 4;     // Temporary value until we know what it actually is
-    private final int NUMCOLS = 4;     // Temporary value until we know what it actually is
-    private TestBoardCell[][] board;
-    private Set<TestBoardCell> targets;
-    private Set<TestBoardCell> visited;
+	private final int NUMROWS = 4;     // Temporary value until we know what it actually is
+	private final int NUMCOLS = 4;     // Temporary value until we know what it actually is
+	private TestBoardCell[][] board;
+	private Set<TestBoardCell> targets;
+	private Set<TestBoardCell> visited;
 
-    public TestBoard() {
-        board = new TestBoardCell[NUMROWS][NUMCOLS]; // Initialize board array
-        for (int row = 0; row < NUMROWS; row++) {
-            for (int col = 0; col < NUMCOLS; col++) {
-                board[row][col] = new TestBoardCell(row, col);
-            }
-        }
-        calcAdj();
-    }
-    
-    public void calcAdj() {
-        for (int row = 0; row < NUMROWS; row++) {
-        	for (int col = 0; col < NUMCOLS; col++) {
-                TestBoardCell cell = board[row][col];
-                if (row < NUMROWS - 1) {
-                	cell.addAdjacency(board[row + 1][col]);
-                }
-        		if (row > 0) {
-        			cell.addAdjacency(board[row - 1][col]); 
-        		}
-                if (col < NUMCOLS - 1) {
-                	cell.addAdjacency(board[row][col + 1]);
-                }
-                if (col > 0) {
-                	cell.addAdjacency(board[row][col - 1]);
-                }
-        	}
-        }
-    }
-    
+	public TestBoard() {
+		board = new TestBoardCell[NUMROWS][NUMCOLS]; // Initialize board array
+		for (int row = 0; row < NUMROWS; row++) {
+			for (int col = 0; col < NUMCOLS; col++) {
+				board[row][col] = new TestBoardCell(row, col);
+			}
+		}
+		calcAdj(); // build adjacency lists
+	}
 
-    public void calcTargets(TestBoardCell startCell, int pathlength) {
-        /*
-            TODO calculates legal targets for a move from startCell of length pathlength.
-            create visited tracker that starts at zero
-            for pathlength
-                get adjacent cells
-                append adjacent cells to visited list maybe
+	// method to build adjacency lists
+	public void calcAdj() {
+		for (int row = 0; row < NUMROWS; row++) {
+			for (int col = 0; col < NUMCOLS; col++) {
+				// check if cell above, below, left or right
+				TestBoardCell cell = board[row][col];
+				if (row < NUMROWS - 1) {
+					cell.addAdjacency(board[row + 1][col]);
+				}
+				if (row > 0) {
+					cell.addAdjacency(board[row - 1][col]); 
+				}
+				if (col < NUMCOLS - 1) {
+					cell.addAdjacency(board[row][col + 1]);
+				}
+				if (col > 0) {
+					cell.addAdjacency(board[row][col - 1]);
+				}
+			}
+		}
+	}
 
-         */
-        Set<TestBoardCell> allVisited = startCell.getAdjList();
-        Set<TestBoardCell> lastVisited = startCell.getAdjList();
-        allVisited.add(startCell);
-        for (int numVisited = 1; numVisited < pathlength; numVisited++) {
-            for (int cell = 0; cell < lastVisited.size(); cell++) {
-            	
-            }
-        }
+	// method to call findAllTargets method
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		targets = new HashSet<>();
+		visited = new HashSet<>();
+		visited.add(startCell);
+		findAllTargets(startCell, pathlength);
+	}
 
-    }
+	// recursive algorithm to find all possible cells can end on
+	private void findAllTargets(TestBoardCell cell, int stepsRemaining) {
+		for (TestBoardCell adj : cell.getAdjList()) {
+			if (visited.contains(adj)) {
+				continue;
+			}
+			if (adj.getOccupied()) {
+				continue;
+			}
+			visited.add(adj);
 
-    /**
-     *
-     * @param row - row of requested cell
-     * @param col - column of requested cell
-     * @return the cell at the requested row/column
-     */
-    public TestBoardCell getCell(int row, int col) {
-        for (int rw = 0; rw < NUMROWS; rw++) {
-            for (int cl = 0; cl < NUMCOLS; cl++) {
-                if (board[rw][cl].row == row && board[rw][cl].col == col) {
-                    return board[rw][cl];
-                }
-            }
-        }
-        System.out.println("ERROR: Cell not found. Check row/col input.");
-        return null;
-    }
+			if (stepsRemaining == 1) {
+				targets.add(adj);
+			} else if (adj.getRoom()){
+				targets.add(adj);
+			} else {
+				findAllTargets(adj, stepsRemaining - 1);
+			}
+			visited.remove(adj);
+		}
+	}
 
-    public Set<TestBoardCell> getTargets() {
-        return targets;
-    }
+	/**
+	 *
+	 * @param row - row of requested cell
+	 * @param col - column of requested cell
+	 * @return the cell at the requested row/column
+	 */
+	public TestBoardCell getCell(int row, int col) {
+		for (int rw = 0; rw < NUMROWS; rw++) {
+			for (int cl = 0; cl < NUMCOLS; cl++) {
+				if (board[rw][cl].row == row && board[rw][cl].col == col) {
+					return board[rw][cl];
+				}
+			}
+		}
+		System.out.println("ERROR: Cell not found. Check row/col input.");
+		return null;
+	}
+
+	public Set<TestBoardCell> getTargets() {
+		return targets;
+	}
 }
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -4,57 +4,57 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TestBoardCell {
-    public final int row;
-    public final int col;
-    private Set<TestBoardCell> adjList;
-    private boolean isRoom;         // Whether the cell is a room
-    private boolean isOccupied;     // Whether the cell is occupied
-     /**
-     * Constructor for creating a cell
-     *
-     * @param r - row of the cell's location
-     * @param c - column of the cell's location
-     */
-    public TestBoardCell(int r, int c) {
-        row = r;
-        col = c;
-        // By default, assume cell is not a room and is not occupied
-        isRoom = false;
-        isOccupied = false;
-        adjList = new HashSet<>();
-        
-    }
+	public final int row;
+	public final int col;
+	private Set<TestBoardCell> adjList;
+	private boolean isRoom;         // Whether the cell is a room
+	private boolean isOccupied;     // Whether the cell is occupied
+	/**
+	 * Constructor for creating a cell
+	 *
+	 * @param r - row of the cell's location
+	 * @param c - column of the cell's location
+	 */
+	public TestBoardCell(int r, int c) {
+		row = r;
+		col = c;
+		// By default, assume cell is not a room and is not occupied
+		isRoom = false;
+		isOccupied = false;
+		adjList = new HashSet<>();
 
-    /**
-     * Adds given cell to this cell's list of adjacent cells
-     *
-     * @param cell - cell to add that is adjacent to this cell
-     */
-    public void addAdjacency(TestBoardCell cell) {
-        adjList.add(cell);
-    }
+	}
 
-    /**
-     * Returns a list of all adjacent cells to this cell
-     */
-    public Set<TestBoardCell> getAdjList() {
-        return adjList;
-    }
+	/**
+	 * Adds given cell to this cell's list of adjacent cells
+	 *
+	 * @param cell - cell to add that is adjacent to this cell
+	 */
+	public void addAdjacency(TestBoardCell cell) {
+		adjList.add(cell);
+	}
 
-    // Setters and Getters
-    public void setRoom(boolean val) {
-        isRoom = val;
-    }
+	/**
+	 * Returns a list of all adjacent cells to this cell
+	 */
+	public Set<TestBoardCell> getAdjList() {
+		return adjList;
+	}
 
-    public boolean getRoom() {
-        return isRoom;
-    }
+	// Setters and Getters
+	public void setRoom(boolean val) {
+		isRoom = val;
+	}
 
-    public void setOccupied(boolean val) {
-        isOccupied = val;
-    }
+	public boolean getRoom() {
+		return isRoom;
+	}
 
-    public boolean getOccupied() {
-        return isOccupied;
-    }
+	public void setOccupied(boolean val) {
+		isOccupied = val;
+	}
+
+	public boolean getOccupied() {
+		return isOccupied;
+	}
 }
