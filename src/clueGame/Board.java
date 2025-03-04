4a9cb1072811da1ec7bd7795dc52d91437529511
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2025-03-03 17:49:27 -0700

    Added all parts of UML diagram to classes

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,14 +1,37 @@
 package clueGame;
 
+import java.util.Map;
+
 public class Board {
 	 /*
      * variable and methods used for singleton pattern
      */
-     private static Board theInstance = new Board();
+	 private BoardCell[][] grid;
+	 private int numRows;
+	 private int numColumns;
+	 private String layoutConfigFile;
+	 private String setupConfigFile;
+	 private Map<Character, Room> roomMap;
+
+	 private static Board theInstance = new Board();
      // constructor is private to ensure only one can be created
      private Board() {
             super() ;
      }
+     
+     
+     public void initalize() {
+    	 
+     }
+     
+     public void loadSetupConfig() {
+    	 
+     }
+     
+     public void loadLayoutConfig() {
+    	 
+     }
+     
      // this method returns the only Board
      public static Board getInstance() {
             return theInstance;
