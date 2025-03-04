08cc15eea282324fcabc3c442fe7212d601686fc
Mark Duffalo <markalarski@yahoo.com>
2025-03-03 21:16:30 -0700

    Formatting: reformatted file with proper spacing/indentation and converted normal comments for function into JavaDoc comments.

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -17,71 +17,99 @@ public class Board {
 	 private static Board theInstance = new Board();
      // constructor is private to ensure only one can be created
      private Board() {
-            super() ;
+            super();
             roomMap = new HashMap<>();
      }
-     // this method returns the only Board
-     public static Board getInstance() {
+
+	/**
+	 *
+	 * @return The instance of the game board. (There is only one instance of Board).
+	 */
+	public static Board getInstance() {
             return theInstance;
      }
-     /*
+
+	/**
       * initialize the board (since we are using singleton pattern)
       */
      public void initialize() {
-    	    grid = new BoardCell[numRows][numColumns]; // Allocate memory
+		grid = new BoardCell[numRows][numColumns]; // Allocate memory
 
-    	    for (int row = 0; row < numRows; row++) {
-    	        for (int col = 0; col < numColumns; col++) {
-    	            grid[row][col] = new BoardCell(row, col);
-    	        }
-    	    }
-    	    loadSetupConfig();
-    	}
+		for (int row = 0; row < numRows; row++) {
+			for (int col = 0; col < numColumns; col++) {
+				grid[row][col] = new BoardCell(row, col);
+			}
+		}
+		loadSetupConfig();
+	}
 
-     
-     public void loadSetupConfig() {
-    	 roomMap.put('S', new Room("Saloon"));
-    	    roomMap.put('J', new Room("Jail"));
-    	    roomMap.put('B', new Room("Bank"));
-    	    roomMap.put('H', new Room("Hotel"));
+	public void loadSetupConfig() {
+		roomMap.put('S', new Room("Saloon"));
+		roomMap.put('J', new Room("Jail"));
+		roomMap.put('B', new Room("Bank"));
+		roomMap.put('H', new Room("Hotel"));
      }
      
      public void loadLayoutConfig() {
     	 
      }
 
+	/**
+	 * Set the files to use for configuration of the game board.
+	 * @param string The name of the config file used for layout.
+	 * @param string2 The name of the config file used for setup.
+	 */
 	public void setConfigFiles(String string, String string2) {
 		// TODO Auto-generated method stub
 	    this.numRows = 28;
 	    this.numColumns = 25;
 	}
 
-
+	/**
+	 *
+	 * @param c The label of the room.
+	 * @return The room object that has the given label.
+	 */
 	public Room getRoom(char c) {
 		
 		// TODO Auto-generated method stub
 		return roomMap.get(c);
 	}
 
-
+	/**
+	 *
+	 * @return The number of rows contained on the board.
+	 */
 	public int getNumRows() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-
+	/**
+	 *
+	 * @return The number of columns contained on the board.
+	 */
 	public int getNumColumns() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-
+	/**
+	 *
+	 * @param i The row of the desired cell.
+	 * @param j The column of the desired cell.
+	 * @return
+	 */
 	public BoardCell getCell(int i, int j) {
 		// TODO Auto-generated method stub
 		return grid[i][j];
 	}
 
-
+	/**
+	 *
+	 * @param cell The cell to be identified.
+	 * @return The room object that the cell is contained within.
+	 */
 	public Room getRoom(BoardCell cell) {
 		// TODO Auto-generated method stub
 		return getRoom(cell.getInitial());
