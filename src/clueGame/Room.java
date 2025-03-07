7610b86f0b25be697328bc291bebe6921b235e71
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2025-03-07 12:47:15 -0700

    Changes made to pass all FileInitTests (need to still do 306 tests and exceptions)

--- src/clueGame/Room.java
+++ src/clueGame/Room.java
@@ -39,11 +39,11 @@ public class Room {
 
 	public BoardCell getLabelCell() {
 		// TODO Auto-generated method stub
-		return null;
+		return labelCell;
 	}
 
 	public BoardCell getCenterCell() {
 		// TODO Auto-generated method stub
-		return null;
+		return centerCell;
 	}
 }
