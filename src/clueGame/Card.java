5097a804d4564fefd7f779a40a8144ab07aad862
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2025-04-01 12:58:55 -0600

    Added all of the empty classes

---
+++ src/clueGame/Card.java
@@ -0,0 +1,13 @@
+package clueGame;
+
+public class Card {
+	private String cardName;
+	
+	public Card() {
+		// some constructor
+	}
+	
+	public boolean equals(Card target) {
+		return false;
+	}
+}
---
+++ src/clueGame/HumanPlayer.java
@@ -0,0 +1,7 @@
+package clueGame;
+
+public class HumanPlayer {
+	public HumanPlayer() {
+		// some constructor
+	}
+}
