df856aca178f9572fc2d9b9566f14dc463769ef3
Mark Duffalo <markduffalo1@gmail.com>
2025-04-03 13:46:57 -0600

    Feature: Added methods to be implemented and refactored existing methods that needed to be abstract.

--- src/clueGame/HumanPlayer.java
+++ src/clueGame/HumanPlayer.java
@@ -1,12 +1,26 @@
 package clueGame;
 
+import java.util.Set;
+
 public class HumanPlayer extends Player {
 	private String type = "HUMAN";
 	
 	public HumanPlayer(String name, String color, int row, int column) {
 		super(name, color, row, column);
 	}
-	
+
+	@Override
+	public Solution createSuggestion() {
+		// TODO: Implement the logic to create a suggestion
+		return null;
+	}
+
+	@Override
+	public Card disproveSuggestion(Solution suggestion) {
+		// TODO: Implement the logic to disprove a suggestion
+		return null;
+	}
+
 	public String getType() {
 		return type;
 	}
