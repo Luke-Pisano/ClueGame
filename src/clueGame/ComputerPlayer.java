a8aa06458de61a4a71a02dd31f929e67bb4eaff9
Mark Duffalo <markduffalo1@gmail.com>
2025-04-03 13:13:20 -0600

    Feature: added selectTarget method to be implemented and createSuggestion with initial implementation that needs to be checked and modified. Also added functionality for seen cards.

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -1,12 +1,51 @@
 package clueGame;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ComputerPlayer extends Player {
 	private String type = "COMPUTER";
+	private Set<Card> seenCards = new HashSet<>();
 
 	public ComputerPlayer(String name, String color, int row, int column) {
 		super(name, color, row, column);
 	}
-	
+
+	public Solution createSuggestion() {
+		// Create a suggestion based on the computer player's hand and the game state
+		// TODO Check implementation of suggestion algorithm
+		Board board = Board.getInstance();
+		Card room = null;
+		Card person = null;
+		Card weapon = null;
+		for (Card card : board.getDeck()) {
+			if (!seenCards.contains(card) && (room == null || person == null || weapon == null)) {
+				if (card.getCardName().equals(board.getRoom(board.getCell(getRow(), getColumn())).getName())) {
+					room = card;
+				} else if (card.getCardType() == CardType.PERSON) {
+					person = card;
+				} else if (card.getCardType() == CardType.WEAPON) {
+					weapon = card;
+				}
+			}
+		}
+		if (room == null || person == null || weapon == null) {
+			// If we can't make a suggestion, return null
+			return null;
+		}
+		return new Solution(room, person, weapon);
+	}
+
+	public BoardCell selectTarget(Set<BoardCell> targets) {
+		// Select a target cell from the set of targets
+		// TODO Implement a strategy for selecting a target
+		return targets.iterator().next();
+	}
+
+	public void updateSeenCards(Card card) {
+		seenCards.add(card);
+	}
+
 	public String getType() {
 		return type;
 	}
