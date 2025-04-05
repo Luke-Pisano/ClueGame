9ec34f1af08590c896486b37459d46ca2cf887e1
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2025-04-04 21:21:02 -0600

    Removed un necessary code

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -51,44 +51,8 @@ public class ComputerPlayer extends Player {
 	    Collections.shuffle(unseenPeople);
 	    Collections.shuffle(unseenWeapons);
 
-	    
-		/*
-		 * 
-		 * Add random ness to select other cards based on random card which hasn't been seen
-		 * Have a set of type Cards of seen.
-		 */
 		
-		return new Solution(roomCard, unseenPeople.get(0), unseenWeapons.get(0));
-
-//		// Get current room based on player's location
-//		BoardCell currentCell = board.getCell(getRow(), getColumn());
-//		String roomName = board.getRoom(currentCell).getName();
-//
-//		Card roomCard = new Card(roomName, CardType.ROOM);
-//		List<Card> unseenPeople = new ArrayList<>();
-//		List<Card> unseenWeapons = new ArrayList<>();
-//
-//		// Loop through the deck
-//		for (Card card : board.getDeck()) {
-//			if (card.getCardType() == CardType.PERSON && !seenCards.contains(card)) {
-//				unseenPeople.add(card);
-//			} else if (card.getCardType() == CardType.WEAPON && !seenCards.contains(card)) {
-//				unseenWeapons.add(card);
-//			}
-//		}
-//
-//		// Make sure we have at least one unseen person and weapon
-//		if (roomCard == null || unseenPeople.isEmpty() || unseenWeapons.isEmpty()) {
-//			return null; // Can't create suggestion
-//		}
-//
-//		// Randomly pick one unseen person and weapon
-//		Collections.shuffle(unseenPeople);
-//		Collections.shuffle(unseenWeapons);
-//
-//		Card person = unseenPeople.get(0);
-//		Card weapon = unseenWeapons.get(0);
-
+		return new Solution(roomCard, unseenPeople.get(0), unseenWeapons.get(0))
 
 	}
 	
