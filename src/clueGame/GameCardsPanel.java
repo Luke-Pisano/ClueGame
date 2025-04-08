320201a2f1f9fbe874ede0062ff80381f2f36915
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2025-04-08 12:26:44 -0600

    Added inital file for cards gui

---
+++ src/clueGame/GameCardsPanel.java
@@ -0,0 +1,23 @@
+package clueGame;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class GameCardsPanel extends JPanel {
+	public GameCardsPanel() {
+		
+	}
+
+
+	public static void main(String[] args) {
+
+	}
+
+}
