dd6c35ad5f857e57dd04b81073db0b329bd4191d
Luke Pisano <79476828+Luke-Pisano@users.noreply.github.com>
2025-04-14 12:41:37 -0600

    created main window with both pannels in ClueGame.java

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -1,6 +1,8 @@
 package clueGame;
 
 import javax.swing.*;
+
+import java.awt.BorderLayout;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Point2D;
 
@@ -11,7 +13,23 @@ public class ClueGame extends JFrame {
     GameControlPanel controlPanel = new GameControlPanel();
 
     public ClueGame() {
+    	setTitle("Clue Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 800);
 
+        board = Board.getInstance();
+        board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+        board.initialize();
+
+        controlPanel = new GameControlPanel();
+        cardsPanel = new GameCardsPanel();
+
+        setLayout(new BorderLayout());
+        add(board, BorderLayout.CENTER);
+        add(controlPanel, BorderLayout.SOUTH);
+        add(cardsPanel, BorderLayout.EAST);
+
+        setVisible(true);
     }
 
     /**
@@ -33,5 +51,9 @@ public class ClueGame extends JFrame {
             cellDim = minDim / board.getNumColumns();
         }
         return cellDim;
+    }
+    
+    public static void main(String[] args) {
+        new ClueGame();
     }
 }
