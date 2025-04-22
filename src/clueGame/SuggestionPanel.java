dbe4c84d7aeee72e88b2761ee6537e863df53ab3
Mark Duffalo <markduffalo1@gmail.com>
2025-04-22 13:37:08 -0600

    Finished implementation of suggestion panel.

--- src/clueGame/SuggestionPanel.java
+++ src/clueGame/SuggestionPanel.java
@@ -1,43 +1,77 @@
 package clueGame;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SuggestionPanel extends JOptionPane {
+public class SuggestionPanel extends JDialog {
     private static final long serialVersionUID = 1L;
-    private static final String title = "Make Your Suggestion";
-    private JPanel panel = new JPanel();
 
     /**
      * Creates a suggestion panel with the specified message and title.
      *
-     * @param message The message to display on the suggestion panel.
      */
-    public SuggestionPanel(String message, Room room) {
-        super(message, JOptionPane.INFORMATION_MESSAGE);
+    public SuggestionPanel(Room room) {
+        setTitle("Make your suggestion");
+        setModal(true);
+        setLayout(new BorderLayout());
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4,1,10,10));
 
         JComboBox<String> roomSuggestion = new JComboBox<>();
         roomSuggestion.setEditable(false);
         roomSuggestion.addItem(room.getName());
+        panel.add(new JLabel("Room:"));
         panel.add(roomSuggestion);
 
         JComboBox<String> playerSuggestion = new JComboBox<>();
+        playerSuggestion.setEditable(false);
         for (Card card : Board.getInstance().getDeck()) {
             if (card.getCardType() == CardType.PERSON) {
                 playerSuggestion.addItem(card.getCardName());
             }
         }
+        panel.add(new JLabel("Person:"));
         panel.add(playerSuggestion);
 
         JComboBox<String> weaponSuggestion = new JComboBox<>();
+        weaponSuggestion.setEditable(false);
         for (Card card : Board.getInstance().getDeck()) {
             if (card.getCardType() == CardType.WEAPON) {
                 weaponSuggestion.addItem(card.getCardName());
             }
         }
+        panel.add(new JLabel("Weapon:"));
         panel.add(weaponSuggestion);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1,2,10,10));
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buttonPanel.add(submitButton);
+        add(panel, BorderLayout.CENTER);
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        buttonPanel.add(cancelButton);
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 
     public static void main(String[] args) {
-        SuggestionPanel suggestion = new SuggestionPanel("Make your suggestion", new Room("Kitchen"));
+        SuggestionPanel suggestion = new SuggestionPanel(new Room("Kitchen"));
+        suggestion.setVisible(true);
     }
 }
