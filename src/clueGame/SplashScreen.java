0085c78c5a2dd0bb3b5e17277a4054a594a14427
Mark Duffalo <markduffalo1@gmail.com>
2025-04-21 20:14:49 -0600

    Added JavaDoc comments to important methods and the constructor since it could be useful.

--- src/clueGame/SplashScreen.java
+++ src/clueGame/SplashScreen.java
@@ -6,11 +6,21 @@ public class SplashScreen extends JOptionPane {
     private static final long serialVersionUID = 1L;
     private String title;
 
+    /**
+     * Creates a splash screen with the specified message and title.
+     *
+     * @param message The message to display on the splash screen.
+     * @param title   The title of the splash screen.
+     */
     public SplashScreen(String message, String title) {
         super(message, JOptionPane.INFORMATION_MESSAGE);
         this.title = title;
     }
 
+    /**
+     * Creates a dialog with the specified title and message.
+     * Used only for testing purposes in main method.
+     */
     public void showSplash() {
         JDialog dialog = createDialog(title);
         dialog.setModal(true);
